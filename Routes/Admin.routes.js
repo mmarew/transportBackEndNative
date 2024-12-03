e9844dccba08b6084f312647ddb729aafd613e74
mmarew@gmail.com <mmarew@gmail.com>
2024-12-03 19:05:18 +0300

    update on api

--- Routes/Admin.routes.js
+++ Routes/Admin.routes.js
@@ -2,7 +2,27 @@ const Router = require("express").Router();
 const AdminController = require("../Controllers/Admin.controller");
 const { verifyAdminsIdentity } = require("../Middleware/verifyUsersIdentity");
 const { verifyTokenOfAxios } = require("../Middleware/verifyToken");
-// Route to get all drivers
+// route to get online drivers
+Router.get(
+  "/api/admin/getOnlineDrivers",
+  verifyTokenOfAxios,
+  verifyAdminsIdentity,
+  AdminController.getOnlineDrivers
+);
+// route to get offline drivers.
+Router.get(
+  "/api/admin/getOfflineDrivers",
+  verifyTokenOfAxios,
+  verifyAdminsIdentity,
+  AdminController.getOfflineDrivers
+);
+// route to get all active drivers meanse user has fulfieled all documents userRolestatus id 1 and role 2
+Router.get(
+  "/api/admin/getAllActiveDrivers",
+  verifyTokenOfAxios,
+  verifyAdminsIdentity,
+  AdminController.getAllActiveDrivers
+);
 
 Router.get(
   "/api/admin/getCompletedJourney",
--- services/Admin.service.js
+++ services/Admin.service.js
@@ -3,6 +3,86 @@ const { pool } = require("../Middleware/Database.config");
 const { getAttachedDocumentsByUser } = require("./AttachedDocuments.service");
 
 const adminServices = {
+  getAllActiveDrivers: async (req) => {
+    // get all active drivers means get data from Users UserRole and UserRoleStatusCurrent. here UserRoleStatusCurrent.statusId = 1 and role id must be 2
+    const sqlToGetAllActiveDrivers = `SELECT 
+    u.*,
+    ur.*,
+    ursc.*
+FROM 
+    Users u
+INNER JOIN 
+    UserRole ur ON u.userUniqueId = ur.userUniqueId
+INNER JOIN 
+    UserRoleStatusCurrent ursc ON ursc.userRoleId = ur.userRoleId
+WHERE 
+    ursc.statusId = 1
+    AND ur.roleId = 2  `;
+    const [data] = await pool.query(sqlToGetAllActiveDrivers);
+    return { message: "success", data: data };
+  },
+  getOfflineDrivers: async (req) => {
+    const sqlToGetOfflineDrivers = `SELECT 
+    dr.*,
+    u.*,
+    ur.*,
+    ursc.*
+FROM 
+    DriverRequest dr
+INNER JOIN (
+    SELECT 
+        userUniqueId, 
+        MAX(requestTime) AS latestRequestTime 
+    FROM 
+        DriverRequest 
+    GROUP BY 
+        userUniqueId
+) latestRequest 
+    ON dr.userUniqueId = latestRequest.userUniqueId 
+    AND dr.requestTime = latestRequest.latestRequestTime
+INNER JOIN 
+    Users u ON dr.userUniqueId = u.userUniqueId
+INNER JOIN 
+    UserRole ur ON ur.userUniqueId = u.userUniqueId
+INNER JOIN 
+    UserRoleStatusCurrent ursc ON ursc.userRoleId = ur.userRoleId
+WHERE 
+    dr.journeyStatusId NOT IN (1, 2, 3, 4)
+    AND ursc.statusId = 1
+    AND ur.roleId = 2  `;
+
+    const [data] = await pool.query(sqlToGetOfflineDrivers);
+    return { message: "success", data: data };
+  },
+
+  getOnlineDrivers: async (req) => {
+    // driver is online when its journeyStatusId is one in DriverRequest table . also get drivers vehicle detailes from vehicle table and VehicleOwnership table. vehicle is connected to VehicleTypes table
+    const data = await performJoinSelect({
+      baseTable: "DriverRequest",
+      joins: [
+        {
+          table: "Users",
+          on: "DriverRequest.userUniqueId = Users.userUniqueId",
+        },
+        {
+          table: "VehicleOwnership",
+          on: "Users.userUniqueId = VehicleOwnership.userUniqueId",
+        },
+        {
+          table: "Vehicle",
+          on: "VehicleOwnership.vehicleUniqueId = Vehicle.vehicleUniqueId",
+        },
+        {
+          table: "VehicleTypes",
+          on: "Vehicle.vehicleTypeUniqueId = VehicleTypes.vehicleTypeUniqueId",
+        },
+      ],
+      conditions: {
+        "DriverRequest.journeyStatusId": 1,
+      },
+    });
+    return { messsage: "success", data };
+  },
   // Fetch completed journeys
   getCompletedJourney: async () => {
     const query = `
