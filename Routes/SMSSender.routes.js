084be797df370ed9ac25838719e66111b5b46e36
mmarew <mmarew@gmail.com>
2025-02-02 18:53:16 +0300

    update on path of ../Controllers/SmsSender.controller

--- Routes/SMSSender.routes.js
+++ Routes/SMSSender.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const smsSenderController = require("../Controllers/smsSender.controller");
+const smsSenderController = require("../Controllers/SmsSender.controller");
 
 // Create a new SMS sender
 router.post("/smsSender", smsSenderController.createSMSSender);
