e9f0c579d4eee605127da54601cb1707e6d16643
mmarew <mmarew@gmail.com>
2025-10-18 14:49:48 +0300

    change redis url to env

--- Utils/Constants.js
+++ Utils/Constants.js
@@ -1,6 +1,6 @@
 //constants;
 
 const REDIS_SOCKET_PATH = "/home/masetawoshacom/tmp/redis.so";
-const UPSTASH_REDIS_URL = `rediss://:AVoEAAIjcDFlYTFmY2U5YzA4NzM0YWNhOThhYzM5YWYwOWIyMGU5ZnAxMA@free-seal-23044.upstash.io:6379`;
-
+const UPSTASH_REDIS_URL = process.env.UPSTASH_REDIS_URL;
+// ("rediss://default:ASiQAAIncDIyZTZjYmRlMTEyM2Y0YmNmYmE0ZjA3ODU0ZWM4NGU3OXAyMTAzODQ@sound-hornet-10384.upstash.io:6379");
 module.exports = { REDIS_SOCKET_PATH, UPSTASH_REDIS_URL };
