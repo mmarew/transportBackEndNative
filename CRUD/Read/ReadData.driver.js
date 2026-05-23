90ad52f84a105b50ba5997c897a809393696afcd
dynamics-tech1 <mmarew@gmail.com>
2026-05-23 16:46:07 +0300

    refactor: split ReadData.js into 5 domain-specific files
    - ReadData.core.js: getData, performJoinSelect
    - ReadData.shipper.js: checkActiveShipperRequest, getShipperRequestByRequestUniqueId
    - ReadData.driver.js: checkActiveDriverRequest, getDriverRequestByRequestUniqueId
    - ReadData.matching.js: findNearbyDrivers, findNearbyShippers
    - ReadData.utils.js: checkUserExists, getCancellationDetails, etc
    - ReadData.js: converted to barrel file to preserve backwards compatibility for existing imports

---
+++ CRUD/Read/ReadData.driver.js
@@ -0,0 +1,93 @@
+const AppError = require('../../Utils/AppError');
+const { transactionStorage } = require('../../Utils/TransactionContext');
+const { pool } = require('../../Middleware/Database.config');
+const { journeyStatusMap } = require('../../Utils/ListOfSeedData');
+const { performJoinSelect } = require('./ReadData.core');
+
+const checkActiveDriverRequest = async (userUniqueId) => {
+  try {
+    // Build placeholders for IN clause
+    const activeStatusPlaceholders = activeJourneyStatuses
+      .map(() => "?")
+      .join(", ");
+
+    const query = `
+      SELECT DISTINCT
+        DriverRequest.*,
+        Users.fullName,
+        Users.phoneNumber,
+        Users.email,
+        JourneyDecisions.isNotSelectedSeenByDriver,
+        JourneyDecisions.isRejectionByShipperSeenByDriver
+      FROM DriverRequest
+      INNER JOIN Users ON DriverRequest.userUniqueId = Users.userUniqueId
+      LEFT JOIN JourneyDecisions ON DriverRequest.driverRequestId = JourneyDecisions.driverRequestId
+      WHERE DriverRequest.userUniqueId = ?
+      AND (
+        -- Active statuses
+        DriverRequest.journeyStatusId IN (${activeStatusPlaceholders})
+        OR
+        -- notSelectedInBid (14) with not seen status
+        (
+          DriverRequest.journeyStatusId = ?
+          AND JourneyDecisions.isNotSelectedSeenByDriver = 'not seen by driver yet'
+        )
+        OR
+        -- Cancellation statuses (7, 10) with not seen status
+        (
+          DriverRequest.journeyStatusId IN (?, ?)
+          AND DriverRequest.isCancellationByShipperSeenByDriver = 'not seen by driver yet'
+        )
+        OR
+        -- rejectedByShipper (8) with not seen status
+        (
+          DriverRequest.journeyStatusId = ?
+          AND JourneyDecisions.isRejectionByShipperSeenByDriver = 'not seen by driver yet'
+        )
+      )
+      ORDER BY DriverRequest.driverRequestId DESC
+      LIMIT 1
+    `;
+
+    const queryExecutor = transactionStorage.getStore() || pool;
+    const [results] = await queryExecutor.query(query, [
+      userUniqueId,
+      ...activeJourneyStatuses,
+      journeyStatusMap.notSelectedInBid,
+      journeyStatusMap.cancelledByShipper,
+      journeyStatusMap.cancelledByAdmin,
+      journeyStatusMap.rejectedByShipper,
+    ]);
+
+    return results; // Returns an array of active requests (if any)
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+const getDriverRequestByRequestUniqueId = async (driverRequestUniqueId) => {
+  const result = await performJoinSelect({
+    baseTable: "DriverRequest",
+    joins: [
+      {
+        table: "Users",
+        on: "DriverRequest.userUniqueId = Users.userUniqueId",
+      },
+    ],
+    conditions: {
+      driverRequestUniqueId: driverRequestUniqueId,
+    },
+  });
+
+  if (!result?.length) {
+    throw new AppError("Request not found", 404);
+  }
+
+  return result[0];
+};
+
+module.exports = {
+  checkActiveDriverRequest,
+  getDriverRequestByRequestUniqueId
+};
---
+++ CRUD/Read/ReadData.utils.js
@@ -0,0 +1,60 @@
+const { pool } = require('../../Middleware/Database.config');
+const { transactionStorage } = require('../../Utils/TransactionContext');
+const { getData } = require('./ReadData.core');
+
+const checkUserExists = async (userUniqueId) => {
+  const existingUser = await getData({
+    tableName: "Users",
+    conditions: { userUniqueId },
+  });
+
+  return existingUser?.length ? existingUser[0] : null;
+};
+//checkActiveShipperRequest is used to get active shipper request from shipper request table, user table ,journey decisions table
+
+
+const getCancellationDetails = async (contextId) => {
+  const result = await performJoinSelect({
+    baseTable: "CanceledJourneys",
+    joins: [
+      {
+        table: "CancellationReasonsType",
+        on: "CanceledJourneys.cancellationReasonsTypeId = CancellationReasonsType.cancellationReasonsTypeId",
+      },
+    ],
+    conditions: {
+      "CanceledJourneys.contextId": contextId,
+    },
+    orderBy: "CanceledJourneys.canceledTime",
+    orderDirection: "DESC",
+    limit: 1,
+  });
+
+  if (!result || result.length === 0) {
+    return null;
+  }
+  return result[0];
+};
+
+
+const getAttachedDocumentsByUserUniqueIdAndDocumentTypeId = async (
+  ownerUserUniqueId,
+  documentTypeId,
+  connection = null,
+) => {
+  const sqlToGetDocument = `select * from AttachedDocuments, DocumentTypes where attachedDocumentCreatedByUserId=? and DocumentTypes.documentTypeId=?`;
+  const values = [ownerUserUniqueId, documentTypeId];
+  const queryExecutor = transactionStorage.getStore() || connection || pool;
+  const [documents] = await queryExecutor.query(sqlToGetDocument, values);
+
+  return {
+    message: "success",
+    data: documents,
+  };
+};
+
+module.exports = {
+  checkUserExists,
+  getCancellationDetails,
+  getAttachedDocumentsByUserUniqueIdAndDocumentTypeId
+};
