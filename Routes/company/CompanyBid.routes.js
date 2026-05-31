73aed4bf5bdebb8d0ff6c8da1707d7d3b6372a28
dynamics-tech1 <mmarew@gmail.com>
2026-05-31 06:37:10 +0300

    update on import file

--- Routes/company/CompanyBid.routes.js
+++ Routes/company/CompanyBid.routes.js
@@ -6,6 +6,7 @@ const controller = require("../../Controllers/CompanyBid.controller");
 const schema = require("../../Validations/CompanyBid.schema");
 const { validator } = require("../../Middleware/Validator");
 const { verifyTokenOfAxios } = require("../../Middleware/VerifyToken");
+const { COMPANY_BID_ENDPOINTS } = require("../EndPoints/companyBid.endpoints");
 
 router.use(verifyTokenOfAxios);
 
@@ -23,7 +24,7 @@ router.get("/", validator(schema.getBidsQuery, "query"), controller.getBids);
  * @route   PATCH /api/company/bids/:companyBidRequestUniqueId/status
  */
 router.patch(
-  "/:companyBidRequestUniqueId/status",
+  COMPANY_BID_ENDPOINTS.UPDATE_BID_STATUS,
   validator(schema.bidParams, "params"),
   validator(schema.updateBidStatus),
   controller.updateBidStatus,
@@ -33,7 +34,7 @@ router.patch(
  * @route   DELETE /api/company/bids/:companyBidRequestUniqueId
  */
 router.delete(
-  "/:companyBidRequestUniqueId",
+  COMPANY_BID_ENDPOINTS.DELETE_BID,
   validator(schema.bidParams, "params"),
   controller.deleteBid,
 );
@@ -44,7 +45,7 @@ router.delete(
  * Call this when the company dispatcher acknowledges the cancellation in the UI.
  */
 router.patch(
-  "/:companyBidRequestUniqueId/markAsSeen",
+  COMPANY_BID_ENDPOINTS.MARK_AS_SEEN,
   validator(schema.bidParams, "params"),
   controller.markCancellationAsSeen,
 );
